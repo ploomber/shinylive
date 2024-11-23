63367ec2d96548b11f48f6ba4b390dc9d8e4005f
Eduardo Blancas <github@blancas.io>
2024-11-23 00:06:14 -0600

    replaces loading animation

--- src/Components/LoadingAnimation.tsx
+++ src/Components/LoadingAnimation.tsx
@@ -4,29 +4,7 @@ import * as React from "react";
 export function LoadingAnimation() {
   return (
     <div className="loading-animation">
-      <div className="hex"></div>
-      <div className="hex"></div>
-      <div className="hex"></div>
-      <br />
-      <div className="hex"></div>
-      <div className="hex"></div>
-      <div className="hex"></div>
-      <div className="hex"></div>
-      <br />
-      <div className="hex"></div>
-      <div className="hex"></div>
-      <div className="spacer"></div>
-      <div className="hex"></div>
-      <div className="hex"></div>
-      <br />
-      <div className="hex"></div>
-      <div className="hex"></div>
-      <div className="hex"></div>
-      <div className="hex"></div>
-      <br />
-      <div className="hex"></div>
-      <div className="hex"></div>
-      <div className="hex"></div>
+      <span className="loading-text">Loading</span>
     </div>
   );
 }
